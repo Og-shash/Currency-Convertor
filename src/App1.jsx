670ce6da2f1353cc1e2b1d443f05621be8b8fbf4
Shashwat Vaish <91148757+Shash-04@users.noreply.github.com>
2024-12-26 11:01:03 +0530

    Update App1.jsx

--- src/App1.jsx
+++ src/App1.jsx
@@ -99,14 +99,15 @@ function App() {
                         </select>
                     </div>
                 </div>
-                <button
-  onClick={swap}
-  className="absolute text-xl sm:text-2xl md:text-3xl font-bold bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200 
-             bottom-16 sm:bottom-20 md:bottom-24 
-             right-10 sm:right-16 md:right-20 lg:right-40"
->
-  SWAP
-</button>
+                <div className="flex justify-center my-4">
+  <button
+    onClick={swap}
+    className="text-xl sm:text-2xl md:text-3xl font-bold bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-600 transition duration-200"
+  >
+    SWAP
+  </button>
+</div>
+
 
                 <div className="bg-white p-3 text-xl rounded-lg my-2">
                     <div>
